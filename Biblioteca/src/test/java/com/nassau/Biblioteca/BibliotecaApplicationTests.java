0df5b831c5ed4cdbaa47f00f02e0604c8c58be9d
arthurleand <arthur.silva@solutis.com.br>
2024-05-14 20:11:20 -0300

    Criar Projeto

---
+++ Biblioteca/src/test/java/com/nassau/Biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nassau.Biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
